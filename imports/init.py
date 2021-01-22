14869df66ac8654145ad5d2c8c7573f49047aead
Lucas <69845439+lucaswillcock@users.noreply.github.com>
2021-01-23 03:53:05 +1300

    cock

--- imports/init.py
+++ imports/init.py
@@ -1,6 +1,7 @@
 import os
 
 def installLibraries():
+    os.system("pip install --upgrade pip")
     os.system("pip3 install kociemba")
     #os.system("pip3 install kociemba")
     #os.system("pip3 install kociemba")

cf04688345ad432b4e9a81fb4cd45496ba2d2df8
Lucas <69845439+lucaswillcock@users.noreply.github.com>
2021-10-08 17:09:26 +1300

    b

--- epaper-example.py
+++ epaper-example.py
@@ -15,32 +15,25 @@ import traceback
 
 try:
     
-    epd = epd4in2.EPD()
-    epd.init()
-    epd.Clear()
+    display = epd4in2.EPD()
+    display.init()
+    display.Clear()
     
     font24 = ImageFont.truetype('Font.ttc', 24)
     font18 = ImageFont.truetype('Font.ttc', 18)
     font35 = ImageFont.truetype('Font.ttc', 35)
     
     # Drawing on the Horizontal image
-    Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+    Himage = Image.new('1', (display.width, display.height), 255)  # 255: clear the frame
     draw = ImageDraw.Draw(Himage)
-    draw.text((10, 0), 'hello world', font = font24, fill = 0)
+    draw.text((10, 0), 'My nuts are huge', font = font24, fill = 0)
     draw.text((10, 20), '4.2inch e-Paper', font = font24, fill = 0)   
     draw.line((20, 50, 70, 100), fill = 0)
-    draw.line((70, 50, 20, 100), fill = 0)
-    draw.rectangle((20, 50, 70, 100), outline = 0)
-    draw.line((165, 50, 165, 100), fill = 0)
-    draw.line((140, 75, 190, 75), fill = 0)
-    draw.arc((140, 50, 190, 100), 0, 360, fill = 0)
-    draw.rectangle((80, 50, 130, 100), fill = 0)
-    draw.chord((200, 50, 250, 100), 0, 360, fill = 0)
-    epd.display(epd.getbuffer(Himage))
+    display.display(display.getbuffer(Himage))
     time.sleep(2)
     
-    epd.Clear()
-    epd.sleep()
+    display.Clear()
+    display.sleep()
     
 except KeyboardInterrupt:    
     epd4in2.epdconfig.module_exit()

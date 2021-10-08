9635b0b78d9f1480ae5400fc0baa7321766d2746
Lucas <69845439+lucaswillcock@users.noreply.github.com>
2021-10-08 18:10:47 +1300

    update

--- epaper-example.py
+++ epaper-example.py
@@ -2,10 +2,6 @@
 # -*- coding:utf-8 -*-
 import sys
 import os
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
-if os.path.exists(libdir):
-    sys.path.append(libdir)
 
 from waveshare_epd import epd4in2
 import time

ad21c2a6f5625b229ed0b5977eca36d0f702a4ec
Lucas <69845439+lucaswillcock@users.noreply.github.com>
2021-10-11 18:32:37 +1300

    ju

--- opencv-webcam.py
+++ opencv-webcam.py
@@ -4,11 +4,15 @@ capture = cv.VideoCapture(0)
 
 while True:
     isTrue,frame = capture.read()
-    frame = cv.flip(frame, 1)
     cv.imshow('Video',frame)
     #cv.imshow("Video 2", frame)
     if cv.waitKey(20) & 0xFF==ord('d'):
         break
+    if cv.waitKey(20) & 0xFF==ord('f'):
+        frame = cv.flip(frame, 1)
+    if cv.waitKey(20) & 0xFF==ord('a'):
+        frame = cv.flip(frame, 0)   
+    
 
 capture.release()
 cv.destroyAllWindows()

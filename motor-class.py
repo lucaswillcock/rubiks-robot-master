d2c9a9220f578e5ccf952980063caa3d632d78b4
Lucas <69845439+lucaswillcock@users.noreply.github.com>
2021-11-01 16:28:55 +1300

    rth

---
+++ motor-class.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import time
+
+enable_PIN = 1
+pulse_PIN = 1
+direction_PIN = 1
+
+class motor:
+    def __init__(self, enable, clock, direction, delay):
+        self.en = enable
+        self.clk = clock
+        self.dir = direction
+        self.dly = delay
+        
+        GPIO.output(self.en, "high")
+        
+    def rotate(self, direction, distance):
+        GPIO.output(self.en, "low")
+        GPIO.output(self.dir, "high")
+        for i in range(50):
+            GPIO.output(self.clk, "high")
+            time.sleep(self.dly)
+            GPIO.output(self.clk, "low")
+            time.sleep(self.dly)
+            
+        GPIO.output(self.en, "high")
+    
+speed = 0.02
+
+Motor = motor("enable", "clock", "direction", speed)
+
+while 1:
+    speed = int(input("Speed: "))
+    Motor.rotate("high", 50)
+    print("Finished")
--- opencv-webcam.py
+++ opencv-webcam.py
@@ -1,25 +1,29 @@
 import cv2 as cv
 import numpy as np
-capture = cv.VideoCapture(0)
+capture0 = cv.VideoCapture(0)
+#capture1 = cv.VideoCapture(1)
 
 while True:
-    isTrue,frame = capture.read()
+    isTrue,frame = capture0.read()
+    #isTrue,frame1 = capture1.read()
     
-    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    lower_blue = np.array([60, 35, 140])
-    upper_blue = np.array([180, 255, 255])
+    #hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    #lower_blue = np.array([60, 35, 140])
+    #upper_blue = np.array([180, 255, 255])
     
-    mask = cv.inRange(hsv, lower_blue, upper_blue)
+    #mask = cv.inRange(hsv, lower_blue, upper_blue)
     
-    frame = cv.bitwise_and(frame, frame, mask = mask)
+    #frame = cv.bitwise_and(frame, frame, mask = mask)
     
-    frame = cv.flip(frame, 1)
+    frame = cv.flip(frame, -1)
+    #frame1 = cv.flip(frame1, 1)
     #frame = cv.rectangle(frame, (20,20), (120, 120), (0, 0, 0))
     #frame = cv.Canny(frame, 100, 100)
     cv.imshow('Video',frame)
+    #cv.imshow("cam 2", frame1)
     if cv.waitKey(20) & 0xFF==ord('d'):
         break
     
 
-capture.release()
+capture0.release()
 cv.destroyAllWindows()

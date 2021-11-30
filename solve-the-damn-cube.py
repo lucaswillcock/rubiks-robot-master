5d48f08b080e3a9db4bf29fd38bf9ce195385e18
Lucas <69845439+lucaswillcock@users.noreply.github.com>
2021-11-30 16:42:04 +1300

    fdfff

--- solve-the-damn-cube.py
+++ solve-the-damn-cube.py
@@ -3,9 +3,11 @@ import logging
 from pyfirmata import *
 import cv2 as cv
 from pyfirmata import Arduino
+from kociemba import *
+from PIL import *
 
 try:
-    #import RPi.GPIO as GPIO
+    import RPi.GPIO as GPIO
     from kociemba import *
 except ModuleNotFoundError as error:
     logging.error(error)
@@ -17,6 +19,7 @@ try:
     logging.info("Arduino connected successfully")
 except serial.serialutil.SerialException as e:
     logging.error(" Couldnt find board")
+    logging.error(e)
 
 #Arduino Pin Assignments
 pulsePin = 10
@@ -49,6 +52,7 @@ class encodedStepper:
     def rotateQuarter(self, direction):
         arduino.digital[self.enable].write(0)
         arduino.digital[self.direction].write(direction)
+        
         for i in range(self.pulsesFullTurn/4):
             arduino.digital[self.pulse].write(1)
             time.sleep(self.speed)
@@ -59,9 +63,24 @@ class encodedStepper:
     def rotateHalf(self, direction):
         arduino.digital[self.enable].write(0)
         arduino.digital[self.direction].write(direction)
+        
         for i in range(self.pulsesFullTurn/2):
             arduino.digital[self.pulse].write(1)
             time.sleep(self.speed)
             arduino.digital[self.pulse].write(1)
             time.sleep(self.speed)
         arduino.digital[self.enable].write(0)
+        
+class cameraObject:
+    def __init__(self, port) -> None:
+        self.camera = cv.VideoCapture(port)
+
+    def getImage(self):
+        _, image = self.camera.read()
+        image = cv.flip(image, 1)
+        cv.putText(image, "Big Fat Cock", (100, 100), cv.FONT_HERSHEY_COMPLEX, 5, (255, 255, 255))
+        cv.imwrite("Image.jpg", image)
+        print("Image capture successful")
+        
+webcam = cameraObject(0)
+webcam.getImage()

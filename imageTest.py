e14608965cf13c54228c4f86aeb2ab388123e9c9
Lucas <69845439+lucaswillcock@users.noreply.github.com>
2022-07-19 18:15:38 +1200

    yfghkjt

--- imageTest.py
+++ imageTest.py
@@ -5,43 +5,51 @@ import imutils
 import numpy as np
 from sklearn.cluster import KMeans
 from collections import Counter
-import pandas as pd 
+import pandas as pd
+import math
 
 image = "topCopy.png"
-
+#process the image to be looked at
 imageTop = cv.imread(image)
 imageTop = imutils.rotate(imageTop, 180)
-#imageTop = cv.cvtColor(imageTop, cv.COLOR_BGR2HSV)
 
-blue = (255, 0, 0)
-lineSize = 2
+#colors as BGR
+white = (255, 255 ,255)
+red = (0, 0, 255)
+green = (0, 255, 0)
+yellow = (0, 255, 255)
+orange = (80, 150, 255)
+blue = (200, 118, 55)
+
+
+lineSize = 1
 
 x = 220
 y = 30
 
 xL = x + 270
 yL = y + 270
-imageTop = cv.rectangle(imageTop, (x, y) , (xL, yL), blue, lineSize)
+imageTop = cv.rectangle(imageTop, (x, y) , (xL, yL), orange, lineSize)
 
-posList = [
-    (235 ,110), 
-    (280, 140),
+listOrange = [
     (320, 165),
-    (235, 165)
+    (280, 140),
+    (235 ,110),
+    (320, 220),
+    (280, 195),
+    (240, 160),
+    (320, 270),
+    (290, 240),
+    (260, 227)
 ]
 
-index=["color", "color_name", "hex", "R", "G", "B"]
-csv = pd.read_csv('colors.csv', names=index, header=None)
+listWhite = [
+    (465, 120)
+]
 
-def recognize_color(R,G,B):
-    minimum = 10000
-    for i in range(len(csv)):
-        d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
-        if(d<=minimum):
-            minimum = d
-            cname = csv.loc[i,"color_name"]
-    return cname
 
+
+#returns dominant colour of image as BGR value
 def get_dominant_color(image, k=4, image_processing_size = None):
     if image_processing_size is not None:
         image = cv.resize(image, image_processing_size, 
@@ -53,32 +61,31 @@ def get_dominant_color(image, k=4, image_processing_size = None):
     dominant_color = clt.cluster_centers_[label_counts.most_common(1)[0][0]]
     return list(dominant_color)
 
-for i in range(len(posList)):
-    XY = posList[i]
-    x = XY[0]
-    y = XY[1]
-    P1 = (x, y)
-    squareSize = 20
-    P2 = (x + squareSize, y + squareSize)
-    
-    #imageTop = cv.rectangle(imageTop, P1, P2, blue, lineSize)
-    
-    section = imageTop[int(P1[1]):int(P2[1]), int(P1[0]) : int(P2[0])]
-    #section = cv.cvtColor(section, cv.COLOR_RGB2HSV)
-    section = cv.resize(section, (200,200))
-    
-    color = get_dominant_color(section)
-    
-    color.reverse()
-    print(color)
-    colourString = recognize_color(color[0], color[1], color[2])
-    print(colourString  )
- 
-# imageTop = imageTop[10:100, 50:110]
+#returns list of colours based on input list of positions
+def getColours(list, image, face):
+    print("Scanning: " + face)
+    for i in range(len(list)):
+        XY = list[i]
+        x = XY[0]
+        y = XY[1]
+        P1 = (x, y)
+        squareSize = 12
+        P2 = (x + squareSize, y + squareSize)
+        
+        image = cv.rectangle(image, P1, P2, blue, lineSize)
+        image = cv.putText(image, str(i), P2, 1, 1, blue)
+        
+        section = image[int(P1[1]):int(P2[1]), int(P1[0]) : int(P2[0])]
+        #section = cv.cvtColor(section, cv.COLOR_RGB2HSV)
+        section = cv.resize(section, (200,200))
+        color = get_dominant_color(section)
+        #color.reverse() #BGR to RGB
+        color = [round(i) for i in color]
+        print(color)
 
-
+getColours(listOrange, imageTop, "Orange")
+getColours(listWhite, imageTop, "White")
 #show result
 cv.imshow("Window", imageTop)
-cv.imshow("Window 2", section)
 cv.waitKey(0)
 cv.destroyAllWindows()

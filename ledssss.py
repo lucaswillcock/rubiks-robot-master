9eb5cd4ece30cdb89d23ab0fd63f24e673e8416b
Lucas <69845439+lucaswillcock@users.noreply.github.com>
2022-07-21 21:28:29 +1200

    w

--- ledssss.py
+++ ledssss.py
@@ -11,9 +11,9 @@ time.sleep(1)
 pixels.fill((50,50,50))
 
 
-while 1:
+for i in range(10):
     for i in range(96):
         
         pixels[i] = (255, 255, 255)
-        time.sleep(0.5)
+        time.sleep(0.1)
         pixels[i] = (0, 0 ,0)

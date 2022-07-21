8ab3717d92b7afd4b38bcc94b4dfc3dea19c0291
Lucas <69845439+lucaswillcock@users.noreply.github.com>
2022-07-21 21:27:30 +1200

    u

--- ledssss.py
+++ ledssss.py
@@ -1,5 +1,6 @@
 import board
 import neopixel
+import time
 
 pixels = neopixel.NeoPixel(board.D18, 96)
 
@@ -8,3 +9,11 @@ pixels.fill((255, 255, 255))
 time.sleep(1)
 
 pixels.fill((50,50,50))
+
+
+while 1:
+    for i in range(96):
+        
+        pixels[i] = (255, 255, 255)
+        time.sleep(0.5)
+        pixels[i] = (0, 0 ,0)

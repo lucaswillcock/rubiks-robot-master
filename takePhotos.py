0c97719531990bd8d9d40d617268b4236132ab23
Lucas <69845439+lucaswillcock@users.noreply.github.com>
2022-07-22 21:19:52 +1200

    s

---
+++ takePhotos.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+
+def takePhoto(port, imageName):
+        camera0 = cv.VideoCapture(port)
+        _, image = camera0.read()
+        #topImg = cv.flip(topImg, 1)
+        cv.imwrite(imageName, image)
+        print("Image capture successful")
+        camera0.release()
+        
+
+takePhoto(0, "bottom.png")

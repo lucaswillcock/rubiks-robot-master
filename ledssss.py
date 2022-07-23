816c52a39731e3e9ded6e76382adb712a0e9bddf
Lucas <69845439+lucaswillcock@users.noreply.github.com>
2022-07-23 21:56:03 +1200

    h

--- ledssss.py
+++ ledssss.py
@@ -1,7 +1,10 @@
 import board
 import neopixel
+import time
 
 leds = neopixel.NeoPixel(board.D18, 96)
+
+leds.fill((255, 255, 255))
 
 class ledRing:
     def __init__(self, motorNum):
@@ -16,3 +19,18 @@ class ledRing:
     def off(self):
         for i in range(len(self.pixels)):
             leds[self.pixels[i]] = (0,0,0)
+            
+            
+ringBack = ledRing(1)
+ringUp = ledRing(2)
+ringLeft = ledRing(3)
+ringDown = ledRing(4)
+ringRight = ledRing(5)
+ringFront = ledRing(6)
+
+ringBack.on((0, 50, 0))
+ringUp.on((0, 0, 50))
+ringLeft.on((50, 0, 50))
+ringDown.on((50, 0, 0))
+ringRight.on((50, 50, 0))
+ringFront.on((0, 50, 50))

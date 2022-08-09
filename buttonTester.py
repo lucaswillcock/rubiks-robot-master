90131e4dd95e7e1c0a7b773cf89d068f4d5761c8
Lucas <69845439+lucaswillcock@users.noreply.github.com>
2022-08-09 19:50:20 +1200

    s

--- buttonTester.py
+++ buttonTester.py
@@ -7,7 +7,11 @@ GPIO.setmode(GPIO.BCM)
 
 lcd = LCD.lcd()
 
+print(GPIO.input(buttonPin))
+
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+print(GPIO.input(buttonPin))
 
 lcd.lcd_display_string("Button not pressed", 1)
 
